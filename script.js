5ecd1df2d37229790b850393ce54eb55a8c58d63
Uyooghene <uyooghene01@gmail.com>
2024-03-09 21:41:32 +0100

    march 09

---
+++ script.js
@@ -0,0 +1,14 @@
+// present Score (if i clkick on the correct button increase by 1)
+// TimeR (if timer is up , let the correct class turn green) and a h1 displays times up
+// question no (random selection when next question button is clickd)
+// question 
+// 4buttons options with a class  that one onclick will be correct
+// next question button that onclick changes the question and adds to present score
+
+let score = 0;
+const start = document.getElementById('start');
+const hall = document.getElementById('hall');
+const correct = document.getElementById('correct');
+const next = document.getElementById('next');
+
+
