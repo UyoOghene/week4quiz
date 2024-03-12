3072d391c55ca274433071841d0cb66368bd999b
Uyooghene <uyooghene01@gmail.com>
2024-03-12 11:02:32 +0100

    march 12

--- script.js
+++ script.js
@@ -6,9 +6,144 @@
 // next question button that onclick changes the question and adds to present score
 
 let score = 0;
+let currentQuestionIndex = 0;
 const start = document.getElementById('start');
-const hall = document.getElementById('hall');
-const correct = document.getElementById('correct');
+const answerButtons = document.getElementById('answer-buttons');
+const question = document.getElementById('question');
 const next = document.getElementById('next');
 
+const questions =[
+    {
+        question:"Which of these is not a starter pokemon?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    },
+    {
+        question:"Which of these has psychic abilities?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    },
+    {
+        question:"Which of these is in its most evolved version?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    },
+    {
+        question:"Which of these moves is supereffective against a water pokemon?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    },
+    {
+        question:"Which of these pokemon does not evolve?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    },
+    {
+        question:"Which of these do i use to increase the level of the pokemon?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    },
+    {
+        question:"Which of these gym leaders specialize in ghost type?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    },
+    {
+        question:"Which of these pokemon has a weakness for pschic moves?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    },
+    {
+        question:"Which pokemon will eevee evolve to if you give her the moon stone?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    },
+    {
+        question:"Which of these moves is supereffective against jynx?",
+        answers: [
+            {text: 'torchic', correct: false},
+            {text: 'cyndaquil', correct: false},
+            {text: 'golem', correct: true},
+            {text: 'totodile', correct: false},
+        ]
+    }
+]
 
+function startQuiz(){
+    currentQuestionIndex = 0 ;
+    score = 0 ;
+    next.innerHTML = 'next';
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+let currentQuestion =questions[currentQuestionIndex];
+let questionNo = currentQuestionIndex +1;
+question.innerHTML =questionNo +'. '+ currentQuestion.question;
+
+currentQuestion.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerHTML = answer.text;
+    button.classList.add('btn');
+    answerButtons.appendChild(button);
+    if(answer.correct){
+        button.dataset.correct =answer.correct;
+    }
+    button.addEventListener('click',selectAnswer);
+    
+});
+}
+
+function resetState(){
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === 'true';
+    if(isCorrect){
+        selectedBtn.classList.add('correct');
+    }else{
+        selectedBtn.classList.add('incorrect');
+    }
+}
+startQuiz();
