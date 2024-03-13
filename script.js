fca99bfdcad771b6637ce251afe56b4906cc58ce
Uyooghene <uyooghene01@gmail.com>
2024-03-13 23:49:22 +0100

    march 13

--- script.js
+++ script.js
@@ -29,80 +29,80 @@ const questions =[
         answers: [
             {text: 'scyther', correct: false},
             {text: 'houndoom', correct: false},
-            {text: 'kadabra', correct: true},
+            {text: 'exeggutor', correct: true},
             {text: 'ivysaur', correct: false},
         ]
     },
     {
         question:"Which of these is in its most evolved version?",
         answers: [
-            {text: 'torchic', correct: false},
-            {text: 'cyndaquil', correct: false},
-            {text: 'golem', correct: true},
-            {text: 'totodile', correct: false},
+            {text: 'bellosom', correct: true},
+            {text: 'nidorina', correct: false},
+            {text: 'golbat', correct: false},
+            {text: 'poliwhirl', correct: false},
         ]
     },
     {
         question:"Which of these moves is supereffective against a water pokemon?",
         answers: [
-            {text: 'torchic', correct: false},
-            {text: 'cyndaquil', correct: false},
-            {text: 'golem', correct: true},
-            {text: 'totodile', correct: false},
+            {text: 'giga drain', correct: true},
+            {text: 'seismic toss', correct: false},
+            {text: 'horn attack', correct: false},
+            {text: 'dreameater', correct: false},
         ]
     },
     {
         question:"Which of these pokemon does not evolve?",
         answers: [
-            {text: 'torchic', correct: false},
-            {text: 'cyndaquil', correct: false},
-            {text: 'golem', correct: true},
-            {text: 'totodile', correct: false},
+            {text: 'kabuto', correct: false},
+            {text: 'dragonair', correct: false},
+            {text: 'tropius', correct: true},
+            {text: 'sentret', correct: false},
         ]
     },
     {
         question:"Which of these do i use to increase the level of the pokemon?",
         answers: [
-            {text: 'torchic', correct: false},
-            {text: 'cyndaquil', correct: false},
-            {text: 'golem', correct: true},
-            {text: 'totodile', correct: false},
+            {text: 'red shard', correct: false},
+            {text: 'black glasses', correct: false},
+            {text: 'moon stone', correct: false },
+            {text: 'rare candy', correct: true},
         ]
     },
     {
-        question:"Which of these gym leaders specialize in ghost type?",
+        question:"Which of these gym leaders specialize in grass type?",
         answers: [
-            {text: 'torchic', correct: false},
-            {text: 'cyndaquil', correct: false},
-            {text: 'golem', correct: true},
-            {text: 'totodile', correct: false},
+            {text: 'lt surge', correct: false},
+            {text: 'misty', correct: false},
+            {text: 'erika', correct: true},
+            {text: 'koga', correct: false},
         ]
     },
     {
         question:"Which of these pokemon has a weakness for pschic moves?",
         answers: [
-            {text: 'torchic', correct: false},
-            {text: 'cyndaquil', correct: false},
-            {text: 'golem', correct: true},
-            {text: 'totodile', correct: false},
+            {text: 'jynx', correct: false},
+            {text: 'tyrogue', correct: true},
+            {text: 'starmie', correct: false},
+            {text: 'bellsprout', correct: false},
         ]
     },
     {
-        question:"Which pokemon will eevee evolve to if you give her the moon stone?",
+        question:"Which pokemon is eevee's psychic evolution?",
         answers: [
-            {text: 'torchic', correct: false},
-            {text: 'cyndaquil', correct: false},
-            {text: 'golem', correct: true},
-            {text: 'totodile', correct: false},
+            {text: 'umbreon', correct: false},
+            {text: 'flareon', correct: false},
+            {text: 'espeon', correct: true},
+            {text: 'glaceon', correct: false},
         ]
     },
     {
         question:"Which of these moves is supereffective against jynx?",
         answers: [
-            {text: 'torchic', correct: false},
-            {text: 'cyndaquil', correct: false},
-            {text: 'golem', correct: true},
-            {text: 'totodile', correct: false},
+            {text: 'earthquake', correct: false},
+            {text: 'water gun', correct: false},
+            {text: 'hyper fang', correct: false},
+            {text: 'bite', correct: true},
         ]
     }
 ]
